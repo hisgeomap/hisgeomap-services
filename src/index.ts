2a7e34b4258a1981c04dab71bd9c86c176e8352c
TreeNewBeeing <zephre0101@gmail.com>
2020-06-11 10:40:54 -0700

    Publish: Ready for 2.0.0.

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import * as Koa from "koa";
 import * as Router from "koa-router";
 import * as bodyParser from "koa-bodyparser";
 import * as cors from "@koa/cors";
-import * as http2 from "http2";
 import * as http from "http";
 import * as fs from "fs";
 import config from "./config";
@@ -13,7 +12,7 @@ const { port, root, SSLport } = config.server;
 const options = {
     key: fs.readFileSync(__dirname + "/../constants/server.key"),
     cert: fs.readFileSync(__dirname + "/../constants/server.crt"),
-    passphrase: "hisgeomap776"
+    passphrase: "hisgeomap776",
 };
 
 const search = new Search();
@@ -21,12 +20,12 @@ const search = new Search();
 const createRouter = () => {
     const app = new Koa();
     const router = new Router();
-    router.get(`${root}`, async ctx => {
+    router.get(`${root}`, async (ctx) => {
         ctx.body = "Hello World!";
     });
 
-    router.get(`${root}/geojson`, async ctx => {
-        const getValue = attr => {
+    router.get(`${root}/geojson`, async (ctx) => {
+        const getValue = (attr) => {
             const attrString = ctx.query[attr];
             if (attrString !== undefined) {
                 return JSON.parse(attrString);
@@ -36,55 +35,56 @@ const createRouter = () => {
         const config = {
             index: {
                 index: ctx.query["index"],
-                type: ctx.query["type"]
+                type: ctx.query["type"],
             },
             filter: getValue("filter"),
             sort: getValue("sort"),
             fields: getValue("fields"),
             aggregation: getValue("aggregation"),
-            size: getValue("size")
+            size: getValue("size"),
         };
 
         const result = await search.searchPlaces(config);
+
         ctx.body = new GeoJSONService().toGeoJSON(result);
     });
 
-    router.get(`${root}/test`, async ctx => {
+    router.get(`${root}/test`, async (ctx) => {
         ctx.body = "Hello World!";
     });
 
     router.post(`${root}/get-years-suggestion`, async (ctx: any) => {
         const { year } = ctx.request.body;
         ctx.body = {
-            year: await search.getYearsAutoComplete(year)
+            year: await search.getYearsAutoComplete(year),
         };
     });
 
     router.post(`${root}/get-years`, async (ctx: any) => {
         const { year } = ctx.request.body;
         ctx.body = {
-            year: await search.getYears(year)
+            year: await search.getYears(year),
         };
     });
 
     router.post(`${root}/search-places`, async (ctx: any) => {
         const { place, year } = ctx.request.body;
         ctx.body = {
-            places: await search.getPlaces(place, year)
+            places: await search.getPlaces(place, year),
         };
     });
 
     router.post(`${root}/search-places-suggestion`, async (ctx: any) => {
         const { place, year } = ctx.request.body;
         ctx.body = {
-            places: await search.getPlacesAutoComplete(place, year)
+            places: await search.getPlacesAutoComplete(place, year),
         };
     });
 
     router.post(`${root}/search-places-year`, async (ctx: any) => {
         const { year, zoom, bound } = ctx.request.body;
         ctx.body = {
-            places: await search.getPlacesByYear(year, zoom, bound)
+            places: await search.getPlacesByYear(year, zoom, bound),
         };
     });
 
@@ -99,9 +99,3 @@ const createRouter = () => {
 const http1App = createRouter();
 http.createServer(http1App.callback()).listen(port);
 console.log(`HTTP 1 Server running on port ${port}.`);
-
-// HTTP 2 Config
-const http2App = createRouter();
-const server = http2.createSecureServer(options, http2App.callback());
-server.listen(SSLport);
-console.log(`HTTP 2 Server running on port ${SSLport}.`);
